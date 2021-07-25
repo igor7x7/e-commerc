577cec8377cf18e6ef5ada97df9c43ac20cb75d5
igor7x7 <80541299+igor7x7@users.noreply.github.com>
2021-07-25 16:59:59 -0300

    segundo commit
    finalizado

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 
 import { Container } from './style'
 
-
 import { Body } from './style'
 
 import api from '../../services/api';
@@ -15,8 +14,12 @@ interface IProduct{
     price: number;
 }
 
+
 const Home: React.FC = () => {
-    const [ data, setData ] = useState<IProduct[]>([]);
+    const [ data, setData ] = useState<IProduct[]>([]);  
+    
+   
+
     useEffect(() => {
         api.get('').then(
             response => {
@@ -29,14 +32,61 @@ const Home: React.FC = () => {
         const productStore = JSON.stringify(data[index])
         localStorage.setItem(`@Produto-${index}`, productStore)
     }
-    
-    const sendForm = (index: any) => {
-        const cadastroStore = JSON.stringify(data[index])
-        localStorage.setItem(`@Cadastro-${index}`, cadastroStore)
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [fone, setFone] = useState('');
+    const [rua, setRua] = useState('');
+    const [numero, setNumero] = useState('');
+    const [complemento, setComplemento] = useState('');
+    const [bairro, setBairro] = useState('');
+    const [cep, setCep] = useState('');
+
+    function addNome(e: any) {
+        setNome(e.target.value)
     }
+    function addEmail(e: any) {
+        setEmail(e.target.value)
+    }
+    function addFone(e: any) {
+        setFone(e.target.value)
+    }
+    function addRua(e: any) {
+        setRua(e.target.value)
+    }
+    function addNumero(e: any) {
+        setNumero(e.target.value)
+    }
+    function addComplemento(e: any) {
+        setComplemento(e.target.value)
+    }
+    function addBairro(e: any) {
+        setBairro(e.target.value)
+    }
+    function addCep(e: any) {
+        setCep(e.target.value)
+    }
+    
+    const cliente ={
+        nome: nome,
+        email: email,
+        fone: fone,
+        rua: rua,
+        numero: numero,
+        complemento: complemento,
+        bairro: bairro,
+        cep: cep,
+    }
+    function chamada(){
+        const chamadaCliente = JSON.stringify(cliente)
+        localStorage.setItem("cliente", chamadaCliente)
+        
+    }
+    
 
 
     return (
+        
         <Body>
             <section>
                 <div className="header-loja">NFT MARKETPLACE</div> 
@@ -58,32 +108,28 @@ const Home: React.FC = () => {
                         <div className="caixa">
                             
                             <div className="text-1">Cadastro</div>
-                            { data.map( (form, index) => (
-                                <div>
-                                    <form>
-                                        <input type="text" id="nome" placeholder="Seu nome" />                                
-                                        <input type="text" id="email" placeholder="E-mail"/>                                
-                                        <input type="text" id="fone" placeholder="Telefone" />
-                                    </form>
-                                    <form className="endereço">
+                                
+                                    <form id="form">
+                                        <input type="text" onChange={ e => addNome(e) } id="nome" placeholder="Seu nome" />                                
+                                        <input type="text" onChange={ e => addEmail(e) } id="email" placeholder="E-mail"/>                                
+                                        <input type="text" onChange={ e => addFone(e) } id="fone" placeholder="Telefone" />
                                         <p>Endereço:</p>
-                                        <input className="rua" type="text" placeholder="Rua" />
-                                        <input type="text" placeholder="Número" />
-                                        <input type="text" placeholder="Complemento" />
-                                        <input type="text" placeholder="Bairro" />
-                                        <input type="text" placeholder="CEP" />
-                                        <input onClick={() => sendForm(index)} id="form" className="button" type="submit" value="Cadastrar"/>
+                                        <input className="rua" onChange={ e => addRua(e) } type="text" placeholder="Rua" />
+                                        <input type="text" onChange={ e => addNumero(e) } id="numero" placeholder="Número" />
+                                        <input type="text" onChange={ e => addComplemento(e) } id="complemento" placeholder="Complemento" />
+                                        <input type="text" onChange={ e => addBairro(e) }id="bairro" placeholder="Bairro" />
+                                        <input type="text" onChange={ e => addCep(e) } id="cep" placeholder="CEP" />
+                                        <input className="button" onClick={ () => chamada()}  type="submit" value="Cadastrar"/>
                                     </form>
-                                </div>
-                            )
-                            )     
-                            }
-                        </div>  
-                    </div>
-                </section>       
-            </Container>
-        </Body>
+                            
+                            </div>  
+                        </div>
+                    </section>       
+                </Container>
        
+            </Body>
+
+            
     );
 
 }
